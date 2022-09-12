8ee80fed427a84204ef3c437d8d87f729dc71838
Anudeep <anudeep.gad@gmail.com>
2022-09-12 14:52:11 -0500

    first commit

---
+++ HW1/1/answers..md
@@ -0,0 +1,16 @@
+## Comment based on the histograms (5)
+
+Both Tex and SPICE occurences look similar.
+
+## What is the frequency of writes (5)
+
+The frequency of writes of TeX and SPICE is approximately 120000 and 80000
+
+## What is the frequency of reads (5)
+
+The frequency of reads of TeX and SPICE is approximately 130000 and 130000
+
+## Please comment on these results (5)
+
+The frequencies of reads is almost similar in both applications. The frequency of
+writes is lower than reads since writing data to memory takes more time
---
+++ HW1/2/matrix-A.js
@@ -0,0 +1,105 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const randomNumbers = (type) => {
+  if (type === "1") return Math.floor(Math.random() * 1000);
+  if (type === "2") return Math.random() * 1000;
+};
+
+/*********************************  
+        Taking input of 1st matrix
+ **********************************************/
+const inputMatrix1 = (rows, columns, type) => {
+  let matrix = new Array(rows).fill(new Array(columns).fill(0));
+  // console.log("Matrix1: ", matrix);
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix[i][j] = randomNumbers(type);
+    }
+  }
+  console.log(matrix);
+
+  return matrix;
+};
+
+/*********************************  
+        Taking input of 2nd matrix
+ **********************************************/
+
+const inputMatrix2 = (rows, columns, type) => {
+  let matrix = new Array(rows).fill(new Array(columns).fill(0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix[i][j] = randomNumbers(type);
+    }
+  }
+
+  return matrix;
+};
+
+/*********************************  
+        Result matrix
+ **********************************************/
+
+const result = (rows, columns) => {
+  let res = new Array(rows).fill(0).map((e) => new Array(columns).fill(0));
+  return res;
+};
+
+/*********************************  
+       Matrix multiplication
+ **********************************************/
+
+const multiplyMatrix = (mat1, mat2, res) => {
+  let sum = 0;
+  for (let i = 0; i < res.length; i++) {
+    for (let j = 0; j < res[i].length; j++) {
+      sum = 0;
+      for (let k = 0; k < mat1[0].length; k++) {
+        sum += mat1[i][k] * mat2[k][j];
+      }
+      res[i][j] = sum;
+    }
+  }
+  return res;
+};
+
+/*********************************  
+ The main function, this is where the code starts execution
+        
+ **********************************************/
+
+const main = () => {
+  const rows1 = Number(prompt("Enter the number of rows of 1st matrix: "));
+  const columns1 = Number(
+    prompt("Enter the number of columns of 1st matrix: ")
+  );
+  const rows2 = Number(prompt("Enter the number of rows of 1st matrix: "));
+  const columns2 = Number(
+    prompt("Enter the number of columns of 1st matrix: ")
+  );
+  if (isNaN(rows1) || isNaN(rows2) || isNaN(columns1) || isNaN(columns2))
+    return "Enter a valid Number, Try Again!";
+
+  if (columns1 !== rows2)
+    return "The number of columns of 1st matrix must match the number of rows of 2nd matrix! Try again";
+
+  const numType = prompt(
+    "Enter the number type PRESS1 : Integer, PRESS2 : Double : "
+  );
+
+  console.log(numType);
+
+  if (numType !== "1" && numType !== "2")
+    return "Enter a valid number type, Try Again!";
+
+  const mat1 = inputMatrix1(rows1, columns1, numType);
+  const mat2 = inputMatrix2(rows2, columns2, numType);
+  const res = result(rows1, columns2);
+  console.log("Matrix1 : ", mat1);
+  console.log("Matrix2 : ", mat2);
+  return multiplyMatrix(mat1, mat2, res);
+};
+
+console.log("Result : ", main());
